5c1195cded4f59c5d9274e1b9aa8ade314acec21
Dédé <maitredede@gmail.com>
2017-09-07 15:16:05 +1100

    Added drawings

---
+++ RaspberryPi.PiGPIO.Drivers.Drawings/ColorConversionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Reflection;
+using System.Text;
+
+namespace PiGPIO.Drivers
+{
+    public static class ColorConversionExtensions
+    {
+        /// <summary>
+        /// Convert ConsoleColor to Color
+        /// </summary>
+        /// <param name="consoleColor"></param>
+        /// <returns></returns>
+        public static Color ToColor(this ConsoleColor consoleColor)
+        {
+            Color c = Color.FromName(consoleColor.ToString());
+            if (c.IsNamedColor)
+                return c;
+            throw new ArgumentException("Unknown color: " + consoleColor.ToString(), paramName: nameof(consoleColor));
+        }
+    }
+}
---
+++ RaspberryPi.PiGPIO.Drivers.Drawings/PWMChannelDriverExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Drawing;
+
+namespace PiGPIO.Drivers
+{
+    public static class PWMChannelDriverExtensions
+    {
+        /// <summary>
+        /// Set PWM for a RGB LED
+        /// </summary>
+        /// <param name="comp">The component</param>
+        /// <param name="ledNum">LED number</param>
+        /// <param name="r">R value</param>
+        /// <param name="g">G value</param>
+        /// <param name="b">B value</param>
+        public static void SetLED(this IPWMChannelDriver comp, int ledNum, int r, int g, int b)
+        {
+            comp.SetPWM(ledNum * 3, r);
+            comp.SetPWM(ledNum * 3 + 1, g);
+            comp.SetPWM(ledNum * 3 + 2, b);
+        }
+
+        public static void SetLED(this IPWMChannelDriver comp, int ledNum, Color color)
+        {
+            comp.SetLED(ledNum, color, 4);
+        }
+
+        public static void SetLED(this IPWMChannelDriver comp, int ledNum, Color color, int bitshift)
+        {
+            comp.SetLED(ledNum, color.R << bitshift, color.G << bitshift, color.B << bitshift);
+        }
+    }
+}
