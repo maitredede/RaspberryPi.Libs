6a18563ea269b34a9784cc08db1a9a9232111f0b
Dédé <maitredede@gmail.com>
2017-10-02 08:09:58 +1100

    Color conversions

---
+++ RaspberryPi.PiGPIO.Drivers.Drawings.ImageSharp/ImageSharpColorConversionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using SixLabors.ImageSharp;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace RaspberryPi.PiGPIO.Drivers
+{
+    public static class ImageSharpColorConversionExtensions
+    {
+        public static Color ToColor(this Rgba32 color)
+        {
+            return Color.FromArgb(color.A, color.R, color.G, color.B);
+        }
+
+        public static Rgba32 ToRgba32(this Color color)
+        {
+            return new Rgba32(color.R, color.G, color.B, color.A);
+        }
+    }
+}
